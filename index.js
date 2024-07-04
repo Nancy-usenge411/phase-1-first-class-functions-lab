697fd2477ab81c9212be3be61854fb964adcdbf2
Nancy Usenge <nancyusenge411@gmail.com>
2024-07-04 23:46:12 +0300

    commit advanced labs

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const returnFirstTwoDrivers =function(drivers){
+    (drivers[ "Mike", "Tom", "Jerry", "Jacob"])
+    return drivers.slice(0,2)
+    }
+    
+    const returnLastTwoDrivers = function(drivers){
+       (drivers["Mike", "Tom", "Jerry", "Jacob"]) 
+       return drivers.slice(-2);
+    }
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+    const createFareMultiplier = function (multiplier){
+        return function(fare){
+            return fare* multiplier;
+        }; 
+    }
+    const fareDoubler = createFareMultiplier (2);
+
+    const fareTripler = createFareMultiplier (3);
+
+    const selectDifferentDrivers = function (arrayOfDrivers, driverFunction){
+        return driverFunction(arrayOfDrivers);
+    }
+
+
 // Code your solution in this file!
